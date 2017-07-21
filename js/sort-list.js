b44f0a3099320960fc8520d431b6eda4acce2395
David Tucker <david@digitalparenting.com>
2017-07-21 11:44:51 -0400

    Adding logging statements

--- js/sort-list.js
+++ js/sort-list.js
@@ -1,10 +1,9 @@
 var t = TrelloPowerUp.iframe();
 
+console.log("Loading Sort List JavaScript");
+
 t.render(function(){
-    // this function we be called once on initial load
-    // and then called each time something changes that
-    // you might want to react to, such as new data being
-    // stored with t.set()
+    console.log("RENDER");
     populateListItems();
 });
 
@@ -18,6 +17,7 @@ var populateListItems = function() {
 };
 
 var handleSortClick = function (e) {
+    console.log("Sort Click");
     if(isSorting) {
         return;
     }
